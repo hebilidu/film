150194e046469dc826e42aedd74770c3e52537d7
lidu <git@hebi.fr>
2021-06-24 17:50:17 +0300

    edit admin.py

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Country, Category, Director, Film
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(Category)
+admin.site.register(Director)
+admin.site.register(Film)
